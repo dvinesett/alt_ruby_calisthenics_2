83f3abcacadbaa9e0d1851a37cac583c974773c0
dvinesett <dvinesett@gmail.com>
2016-09-22 11:23:16 -0400

    caching test

--- autograder/fun_with_strings_spec.rb
+++ autograder/fun_with_strings_spec.rb
@@ -1,4 +1,4 @@
-describe 'palindrome detection' do
+describe 'palindrome detection (cache test)' do
   it 'should work for simple strings [10 points]' do
     'redivider'.should  be_a_palindrome
     'abracadabra'.should_not  be_a_palindrome
